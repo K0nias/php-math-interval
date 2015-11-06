16bfc9529fd1aca649bdc79fa768b74ede090531
Petr Hejna <Achse0@gmail.com>
2015-11-06 23:08:37 +0100

    test fix bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,8 +5,15 @@ namespace Achse\Math\Interval\Test;
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/Types/TestComparison.php';
 
+use Tester\Environment;
+
+
+
 if (!class_exists('Tester\Assert')) {
 	echo "Install Nette Tester using `composer update --dev`\n";
 	exit(1);
 }
 
+date_default_timezone_set('Europe/Prague');
+
+Environment::setup();
